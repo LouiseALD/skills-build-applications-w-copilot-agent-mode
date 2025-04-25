68bad6c25a96bbd1054619a309d69551e2052ac4
Louise Aldrighi <48850135+LouiseALD@users.noreply.github.com>
2025-04-25 00:08:10 +0000

    fix(octofit): comando populate_db.py usando ORM do Django para avaliação

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,52 +1,42 @@
 from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from octofit_tracker.test_data import test_users, test_teams, test_activities, test_leaderboard, test_workouts
-import pymongo
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Command(BaseCommand):
     help = 'Popula o banco octofit_db com dados de teste para users, teams, activities, leaderboard e workouts.'
 
     def handle(self, *args, **kwargs):
-        client = pymongo.MongoClient('mongodb://localhost:27017/')
-        db = client['octofit_db']
-
         # Limpa as coleções
-        for col in ['users', 'teams', 'activities', 'leaderboard', 'workouts']:
-            db[col].delete_many({})
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
 
         # Popula usuários
         for u in test_users:
-            db['users'].insert_one(u)
+            User.objects.create(email=u['email'], name=u['name'], password=u['password'])
 
         # Popula times
         for t in test_teams:
-            db['teams'].insert_one(t)
+            Team.objects.create(name=t['name'], members=t['members'])
 
         # Popula atividades
         for a in test_activities:
-            doc = {
-                'user_email': a['user_email'],
-                'activity_type': a['type'],
-                'duration': a['duration'],
-                'distance': a.get('distance', 0.0),
-                'date': datetime(2025, 4, 20, 8, 0)
-            }
-            db['activities'].insert_one(doc)
+            Activity.objects.create(
+                user_email=a['user_email'],
+                activity_type=a['type'],
+                duration=timedelta(minutes=a['duration']),
+                date=datetime(2025, 4, 20, 8, 0)
+            )
 
         # Popula leaderboard
         for l in test_leaderboard:
-            doc = {
-                'team_name': l.get('team', ''),
-                'score': l['score']
-            }
-            db['leaderboard'].insert_one(doc)
+            Leaderboard.objects.create(user_email=l.get('team', ''), score=l['score'])
 
         # Popula workouts
         for w in test_workouts:
-            doc = {
-                'description': w['description'],
-                'suggested_for': w.get('suggested_for', 'all')
-            }
-            db['workouts'].insert_one(doc)
+            Workout.objects.create(description=w['description'], suggested_for=w.get('suggested_for', 'all'))
 
-        self.stdout.write(self.style.SUCCESS('Banco octofit_db populado com dados de teste via comando Django!'))
+        self.stdout.write(self.style.SUCCESS('Banco octofit_db populado com dados de teste!'))
